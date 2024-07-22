8a9f21dc1d0bfbe93d8fcf93558fc0c365886c57
Ruben Avoian <rubenavoian@gmail.com>
2024-07-21 18:24:41 -0700

    add initial files

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace SlackMessageBot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string token = "<slack_bot_token_here>";
+
+            // Basic command-line interface
+            Console.WriteLine("Enter your desired message to post to Slack:");
+            string input = Console.ReadLine();
+
+            // Parameters for API call
+            var payload = new
+            {
+                channel = "<slack_channel_id_here>",
+                text = input,
+            };
+            string serializedPayload = JsonSerializer.Serialize(payload);
+
+            // Setup request client & header
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+            // Post the request & display output
+            StringContent body = new StringContent(serializedPayload, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await httpClient.PostAsync("https://slack.com/api/chat.postMessage", body);
+            Console.WriteLine(response);
+
+            // Display output for message status
+            string responseContent = await response.Content.ReadAsStringAsync();
+            JsonDocument parsedContent = JsonDocument.Parse(responseContent);
+            string contentFormatted = JsonSerializer.Serialize(parsedContent, new JsonSerializerOptions { WriteIndented = true });
+            Console.WriteLine(contentFormatted);
+        }
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# C# Slack Message Bot
+
+Sample C# console project for posting to using a designated Slack channel using the Slack App API
+
+## For running locally:
+
+1. Clone or download this repository [Repository | GitLab](https://docs.gitlab.com/ee/user/project/repository/)
+
+2. Install Visual Studio [Your Privacy Choices Opt-Out Icon](https://visualstudio.microsoft.com/downloads/)
+
+3. Open `SlackMessageBot.csproj`
+
+4. Run project
+
+## Additional online resources:
+
+- [chat.postMessage method | Slack](https://api.slack.com/methods/chat.postMessage)
+- [Welcome Bot | Slack](https://api.slack.com/tutorials/tracks/create-bot-to-welcome-users)
